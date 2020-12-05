3d97f848b637abc7e19b73e9c855017ee0a640f1
yang.yang <yang.yang@shgpu-t4-005.xforwardai.io>
2020-12-05 20:02:34 +0800

    add some func

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/JackSliwen/go_UtilityFunc/calculate"
+	"github.com/JackSliwen/go_UtilityFunc/file"
+	"github.com/JackSliwen/go_UtilityFunc/image"
+)
+
+func main() {
+	// 输出hello world
+	fmt.Println("Hello Wrold")
+}
